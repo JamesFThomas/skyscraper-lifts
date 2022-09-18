07a979a5a8a809ce7e392d97144046e261244acd
JamesFThomas <james.fthomas@yahoo.com>
2022-09-18 04:32:29 -0500

    basic information added to About component

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,16 +1,31 @@
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 
 const About = () => {
-    return (
-        <div className="page">
-            <Container>
-                <div>
-                    {"About"}
-                </div>
-            </Container>
+  return (
+    <div className='page'>
+      <Container>
+        <div>
+          <h1>{'About This Application'}</h1>
+          <div>
+            <h2>{'Application Objective'}</h2>
+            <p>
+              {`
+                To design an elevator system for a building containing 100 floors.
+                This system should minimize the amount of time spent between
+                calling an elevator and arriving at the destination floor`}
+            </p>
+          </div>
+          <div>
+            <h3>{'Application Modes'}</h3>
+            <Button>{'Single'}</Button> <Button>{'Auto'}</Button>
+            <h3>{'Application Constraints'}</h3>
+            <Button>{'Challenges'}</Button>
+          </div>
         </div>
-
-    );
-}
+      </Container>
+    </div>
+  );
+};
 
 export default About;
