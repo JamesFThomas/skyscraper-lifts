ce78ad2d5ecd910a5daa8e4c6002db79c5bc2803
JamesFThomas <james.fthomas@yahoo.com>
2022-09-22 19:55:31 -0500

    removed conditional render for class name change

--- src/components/lift/CallPanel.js
+++ src/components/lift/CallPanel.js
@@ -3,34 +3,29 @@ import { Container } from 'react-bootstrap';
 import '../../styles/callPanel.css';
 
 const CallPanel = () => {
-  const [upClick, setUpClick] = useState(false);
-  const [downClick, setDownClick] = useState(false);
+  const [upClick, setUpClick] = useState('');
+  const [downClick, setDownClick] = useState('');
 
   const handelUp = (e) => {
     e.preventDefault();
-    setUpClick(!upClick);
+    upClick === '' ? setUpClick('-fill') : setUpClick('');
   };
   const handelDown = (e) => {
     e.preventDefault();
-    setDownClick(!downClick);
+    downClick === '' ? setDownClick('-fill') : setDownClick('');
   };
 
   return (
     <Container className='panel'>
-      {upClick ? (
-        <span class='arrow bi bi-caret-up' onClick={(e) => handelUp(e)} />
-      ) : (
-        <span class='arrow bi bi-caret-up-fill' onClick={(e) => handelUp(e)} />
-      )}
+      <span
+        class={`arrow bi bi-caret-up${upClick}`}
+        onClick={(e) => handelUp(e)}
+      />
       <div className='panelText'>CALL</div>
-      {downClick ? (
-        <span
-          class='arrow bi bi-caret-down-fill'
-          onClick={(e) => handelDown(e)}
-        />
-      ) : (
-        <span class='arrow bi bi-caret-down' onClick={(e) => handelDown(e)} />
-      )}
+      <span
+        class={`arrow bi bi-caret-down${downClick}`}
+        onClick={(e) => handelDown(e)}
+      />
     </Container>
   );
 };
