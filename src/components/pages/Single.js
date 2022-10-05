657c2bed2ef5ffd1ff3c8c7ef270289baebb5488
JamesFThomas <james.fthomas@yahoo.com>
2022-10-05 16:37:42 -0500

    controlButtons.js created for system control

--- src/components/pages/Single.js
+++ src/components/pages/Single.js
@@ -3,12 +3,13 @@ import Container from 'react-bootstrap/Container';
 import CallPanel from '../lift/CallPanel';
 import DisplayPanel from '../lift/DisplayPanel';
 import LiftDoors from '../lift/LiftDoors';
+import ControlButtons from '../lift/ControlButtons';
 
 const Single = () => {
   return (
     <div className='page'>
       <Container>
-        <div>{'Single'}</div>
+        <ControlButtons />
         <CallPanel />
         <DisplayPanel />
         <LiftDoors />
