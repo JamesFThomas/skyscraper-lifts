66be635ed81df4e706885c18a0356b934360b5b3
JamesFThomas <james.fthomas@yahoo.com>
2022-10-13 10:41:17 -0500

    displaySlice moveLift function built, not working

--- src/components/lift/DisplayPanel.js
+++ src/components/lift/DisplayPanel.js
@@ -1,31 +1,32 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { goingUp, goingDown } from '../redux/displaySlice';
+import { moveLift } from '../redux/displaySlice';
 import '../../styles/displayPanel.css';
 
 const DisplayPanel = () => {
-  const [goingUp, setGoingUp] = useState(false);
-  const [goingDown, setGoingDown] = useState(false);
+  const dUp = useSelector((state) => state.display.dUp);
+  const dDown = useSelector((state) => state.display.dDown);
   const currentFloor = useSelector((state) => state.display.currentFloor);
-
-  const showUp = () => {
-    goingUp ? setGoingUp('-fill') : setGoingUp('');
-  };
-  const showDown = () => {
-    goingDown ? setGoingDown('-fill') : setGoingDown('');
-  };
+  const dispatch = useDispatch();
 
   return (
     <div className='disPanel'>
       <div className='disArrow'>
-        <span className={`arrow bi bi-caret-up ${goingUp}`} />
+        <span
+          className={dUp ? 'arrow bi bi-caret-up-fill' : 'arrow bi bi-caret-up'}
+        />
       </div>
       <div className='floorDisplay '>
         <div className='displayText'>{currentFloor}</div>
       </div>
       <div className='disArrow'>
-        <span className={`arrow bi bi-caret-down ${goingDown}`} />
+        <span
+          className={
+            dDown ? 'arrow bi bi-caret-down-fill' : 'arrow bi bi-caret-down'
+          }
+        />
       </div>
+      <button onClick={() => dispatch(moveLift(60))}>Run</button>
     </div>
   );
 };
--- src/components/redux/displaySlice.js
+++ src/components/redux/displaySlice.js
@@ -3,46 +3,96 @@ import { createSlice } from '@reduxjs/toolkit';
 export const displaySlice = createSlice({
   name: 'displayPanel',
   initialState: {
-    currentFloor: 77,
-    startFloor: 0,
-    endFloor: 0,
+    currentFloor: 50,
+    startFloor: undefined,
+    endFloor: undefined,
     dUp: false,
     dDown: false,
+    trips: [],
   },
   reducers: {
-    //function to set goingUp bool
-    goingUp: (state) => {
-      if (state.dUp) {
-        state.dUp = false;
-      }
-      state.dUp = true;
+    //function to set dUp bool
+    showUp: (state, action) => {
+      state.dUp = action.payload;
     },
-    //function to set goingDown bool
-    goingDown: (state) => {
-      if (state.dDown) {
-        state.dDown = false;
-      }
-      state.dDown = true;
+    //function to set dDown bool
+    showDown: (state, action) => {
+      state.dUp = action.payload;
     },
     // function to update current floor
-    setFloor: (state, action) => {
+    setCurFloor: (state, action) => {
       state.currentFloor += action.payload;
+    },
+    // function to update ending floor
+    setEndFloor: (state, action) => {
+      state.endFloor += action.payload;
+    },
+    // function to update ending floor
+    setStartFloor: (state, action) => {
+      state.startFloor += action.payload;
+    },
+    trackTrip: (state, action) => {
+      state.trips.push(action.payload);
+    },
+    //dispatch
+    moveLift: (state, action) => {
+      //set ending floor from action payload
+      let end = action.payload;
+      setEndFloor(end);
+      // track starting floor
+      setStartFloor(state.currentFloor);
+      //move lift down if action.payload < currentFloor
+      if (state.endFloor < state.currentFloor) {
+        showUp(true);
+        while (state.currentFloor <= state.endFloor) {
+          setTimeout(() => {
+            setCurFloor(1);
+          }, 1000);
+        }
+        if (state.currentFloor === state.endFloor) {
+          showUp(false);
+          let trip = {
+            start: state.startFloor,
+            end: state.endFloor,
+          };
+          trackTrip(trip);
+          setEndFloor(undefined);
+          setStartFloor(undefined);
+        }
+      }
+      //move lift up if action.payload > currentFloor
+      if (state.endFloor > state.currentFloor) {
+        showDown(true);
+        while (state.currentFloor >= state.endFloor) {
+          setTimeout(() => {
+            setCurFloor(-1);
+          }, 1000);
+        }
+        if (state.currentFloor === state.endFloor) {
+          showDown(false);
+          let trip = {
+            start: state.startFloor,
+            end: state.endFloor,
+          };
+          trackTrip(trip);
+          setEndFloor(undefined);
+          setStartFloor(undefined);
+        }
+      }
     },
   },
 });
 
 // create actions for this slice
-export const { goingUp, goingDown, setFloor } = displaySlice.actions;
-
-//create async function that using setTimeout to update currentFloor
-export const moveLiftAsync = (end) => (dispatch) => {
-  //check if end === currentFloor
-  //throw error
-  //check if end < currentFloor
-  //setTimeout decrease current floor until === end
-  //check if end > currentFloor
-  //setTimeout increase current floor until === end
-};
+export const {
+  showUp,
+  showDown,
+  setCurFloor,
+  setEndFloor,
+  setStartFloor,
+  trackTrip,
+  moveLift,
+} = displaySlice.actions;
 
 // export reducer for this slice to store
 export default displaySlice.reducer;
