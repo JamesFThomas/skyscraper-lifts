0c99726b675848fc6a0b34b1bb5e656908c8d453
JamesFThomas <james.fthomas@yahoo.com>
2022-10-24 10:04:32 -0500

    floorPanel comp styled with outlined buttons

--- src/components/lift/FloorPanel.js
+++ src/components/lift/FloorPanel.js
@@ -1,5 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
 
-const FloorPanel = () => {};
+import '../../styles/floorPanel.css';
+
+const FloorPanel = () => {
+  const [floors] = useState([...Array(100).keys()]);
+  return (
+    <div className='floorGrid'>
+      {floors.map((floor, i) => (
+        <Button
+          className='gridButton'
+          key={i}
+          value={floor}
+          variant='outline-secondary'
+          size='sm'
+        >
+          {floor}
+        </Button>
+      ))}
+    </div>
+  );
+};
 
 export default FloorPanel;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,13 @@
 import Container from 'react-bootstrap/Container';
 import Counter from '../lift/Counter';
+import FloorPanel from '../lift/FloorPanel';
 
 const Home = () => {
   return (
     <div className='page'>
       <Container>
         <div>{'Home'}</div>
-        <Counter />
+        <FloorPanel />
       </Container>
     </div>
   );
