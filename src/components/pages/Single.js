c36d91122484071af29c961ddb8c77acc3940d4c
JamesFThomas <james.fthomas@yahoo.com>
2022-10-25 08:52:13 -0500

    imported new state values for conditional render

--- src/components/pages/Single.js
+++ src/components/pages/Single.js
@@ -1,14 +1,22 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 import CallPanel from '../lift/CallPanel';
+import FloorPanel from '../lift/FloorPanel';
 import DisplayPanel from '../lift/DisplayPanel';
 import LiftDoors from '../lift/LiftDoors';
 import ControlButtons from '../layout/ControlButtons';
 import TripLog from '../layout/TripLog';
 
 const Single = () => {
+  //import useSelector and track state for rendering
+  const IDLE = useSelector((state) => state.display.IDLE);
+  const SELECT = useSelector((state) => state.display.SELECT);
+  const MOVING = useSelector((state) => state.display.MOVING);
+
   return (
     <div className='page'>
       <Container>
@@ -33,7 +41,7 @@ const Single = () => {
                 alignItems: 'center',
               }}
             >
-              <Col md={{ span: 8, offset: 1 }} >
+              <Col md={{ span: 8, offset: 1 }}>
                 <DisplayPanel />
               </Col>
             </Row>
