213460671e981c8af0c10a0cd074242218eb97cc
James F. Thomas <jthomas@rsimail.com>
2024-02-07 07:47:45 -0600

    updated SimulatorSummary functions to state values

--- src/components/simulator/SimulatorSummary.js
+++ src/components/simulator/SimulatorSummary.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { Button, Stack, Typography } from "@mui/material";
 import { useSelector, useDispatch } from "react-redux";
 
@@ -9,32 +10,59 @@ const SimulatorSummary = () => {
   const L2ridesArr = useSelector((state) => state.everyLift.lift2.trips);
   const L3ridesArr = useSelector((state) => state.everyLift.lift3.trips);
 
+  const dispatch = useDispatch();
+
   //TODO turn into state variables
+  const [allElevatorRides] = useState([
+    ...L1ridesArr,
+    ...L2ridesArr,
+    ...L3ridesArr,
+  ]);
+
+  const [rideTimes, setRideTimes] = useState({
+    enroute: 0,
+    taxing: 0,
+    total: 0,
+  });
+
+  const [enrouteRides, setEnrouteRides] = useState(0);
+  const [taxingRides, setTaxingRides] = useState(0);
+  const [totalAvgWait, setTotalAvgWait] = useState();
+  const [totalAvgInLift, setTotalAvgInLift] = useState();
+  const [totalAvgDuration, setTotalAvgDuration] = useState();
+
   const totalTimes = {
     enroute: 0,
     taxing: 0,
     total: 0,
   };
-  let eRides = 0;
-  let tRides = 0;
-  const allRides = [...L1ridesArr, ...L2ridesArr, ...L3ridesArr];
-  const TotalAvgWait = totalTimes.enroute / eRides;
-  const TotalAvgInLift = totalTimes.taxing / tRides;
-  const TotalAvgDuration = totalTimes.total / allRides.length;
-  const dispatch = useDispatch();
 
-  // TODO place in useEffect
-  allRides.forEach((ride) => {
-    if (ride.passengers === 0) {
-      totalTimes.enroute += ride.duration;
-      eRides += 1;
-    }
-    if (ride.passengers > 0) {
-      totalTimes.taxing += ride.duration;
-      tRides += 1;
-    } else {
-      totalTimes.total += ride.duration;
-    }
+  useEffect(() => {
+    if (allElevatorRides.length > 0)
+      allElevatorRides.forEach((ride) => {
+        if (ride.passengers === 0) {
+          setRideTimes((state) => {
+            return { ...state, enroute: (state.enroute += ride.duration) };
+          });
+          setEnrouteRides((prev) => (prev += 1));
+        }
+        if (ride.passengers > 0) {
+          setRideTimes((state) => {
+            return { ...state, taxing: (state.taxing += ride.duration) };
+          });
+          setTaxingRides((prev) => (prev += 1));
+        } else {
+          totalTimes.total += ride.duration;
+          setRideTimes((state) => {
+            return { ...state, total: (state.total += ride.duration) };
+          });
+        }
+      });
+    setTotalAvgWait(Math.round((rideTimes.enroute / enrouteRides) * 100) / 100);
+    setTotalAvgInLift(Math.round((rideTimes.taxing / taxingRides) * 100) / 100);
+    setTotalAvgDuration(
+      Math.round((rideTimes.total / (enrouteRides + taxingRides)) * 100) / 100
+    );
   });
 
   const handleClick = () => {
@@ -44,16 +72,10 @@ const SimulatorSummary = () => {
 
   return (
     <Stack spacing={3}>
-      <Typography>{`Average Wait Time: ${TotalAvgWait.toFixed(
-        2
-      )} seconds`}</Typography>
-      <Typography>{`Average Time in Lift: ${TotalAvgInLift.toFixed(
-        2
-      )} seconds`}</Typography>
+      <Typography>{`Average Wait Time: ${totalAvgWait} seconds`}</Typography>
+      <Typography>{`Average Time in Lift: ${totalAvgInLift} seconds`}</Typography>
       <Typography>
-        {`Average duration of all rides: ${TotalAvgDuration.toFixed(
-          2
-        )} seconds`}
+        {`Average duration of all rides: ${totalAvgDuration} seconds`}
       </Typography>
       <Button variant="contained" onClick={handleClick}>
         {" "}
