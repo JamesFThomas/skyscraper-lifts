10bc7dc79ed92fb58cd225e5a653f9050da68707
James F. Thomas <jthomas@rsimail.com>
2024-09-25 15:41:13 -0500

    DirectionPanel testing suite - all tests passing

--- src/__tests__/Simulator/DirectionPanel.test.js
+++ src/__tests__/Simulator/DirectionPanel.test.js
@@ -1,13 +1,48 @@
-import renderer from "react-test-renderer";
-import DirectionPanel from "../../components/simulator/liftParts/DirectionPanel";
+import { renderWithProviders } from '../../utils/TestingWrapper';
+import { screen, waitFor } from '@testing-library/react';
+import DirectionPanel from '../../components/simulator/liftParts/DirectionPanel';
 
-//TODO add tests for display text
+const mockDownProps = {
+  direction: 'DOWN',
+  currentFloor: '28',
+};
 
-describe("DirectionPanel testing suite", () => {
-  it("Component successfully renders", () => {
-    const component = renderer.create(
-      <DirectionPanel props={{ direction: "DOWN", currentFloor: "28" }} />
+const mockUpProps = {
+  direction: 'UP',
+  currentFloor: '82',
+};
+
+describe('DirectionPanel testing suite', () => {
+  it('Component successfully renders', () => {
+    renderWithProviders(<DirectionPanel props={mockUpProps} />);
+
+    const panel = screen.getByTestId('DirectionalPanel-container');
+    expect(panel).toBeTruthy();
+  });
+
+  it('Component renders current floor when passed', async () => {
+    renderWithProviders(<DirectionPanel props={mockUpProps} />);
+
+    await waitFor(() => {
+      screen.getByTestId('DirectionalPanel-currentFloor');
+    });
+
+    const currentFloor = screen.getByTestId('DirectionalPanel-currentFloor');
+
+    expect(currentFloor).toHaveStyle(
+      'border: 2px solid grey; font-size: 32px; padding: 16px; display: flex; flex-direction: row; justify-content: center',
     );
-    expect(component).toBeTruthy();
+  });
+
+  it('Component renders up icon when prop is true', () => {
+    renderWithProviders(<DirectionPanel props={mockUpProps} />);
+    const iconBox = screen.getByTestId('DirectionalPanel-upIconBox');
+    expect(iconBox).toBeInTheDocument();
+  });
+
+  it('Component renders down icon when prop is true', () => {
+    renderWithProviders(<DirectionPanel props={mockDownProps} />);
+    const iconBox = screen.getByTestId('DirectionalPanel-downIconBox');
+    expect(iconBox).toBeInTheDocument();
   });
 });
--- src/components/simulator/liftParts/DirectionPanel.js
+++ src/components/simulator/liftParts/DirectionPanel.js
@@ -1,41 +1,41 @@
-import { Box, Stack, Typography } from "@mui/material";
-import { Forward, ForwardOutlined } from "@mui/icons-material";
+import { Box, Stack, Typography } from '@mui/material';
+import { Forward, ForwardOutlined } from '@mui/icons-material';
 
 const stackStyles = () => {
   return {
-    border: "2px solid grey",
-    width: "fit-content",
+    border: '2px solid grey',
+    width: 'fit-content',
   };
 };
 
 const textStyles = () => {
   return {
-    border: "2px solid grey",
+    border: '2px solid grey',
     fontSize: 32,
-    padding: "16px",
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
+    padding: '16px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
   };
 };
 
 const iconBoxStyles = () => {
   return {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
   };
 };
 
 const upIconStyles = () => {
   return {
-    transform: "rotate(270deg)",
+    transform: 'rotate(270deg)',
     fontSize: 30,
   };
 };
 const downIconStyles = () => {
   return {
-    transform: "rotate(90deg)",
+    transform: 'rotate(90deg)',
     fontSize: 30,
   };
 };
@@ -43,20 +43,27 @@ const downIconStyles = () => {
 const DirectionPanel = (props) => {
   const { direction, currentFloor } = props;
 
-  const up = direction === "UP";
-  const down = direction === "DOWN";
+  const up = direction === 'UP';
+  const down = direction === 'DOWN';
 
   return (
-    <Stack padding={2} spacing={2} sx={stackStyles}>
-      <Box sx={iconBoxStyles}>
+    <Stack
+      data-testid="DirectionalPanel-container"
+      padding={2}
+      spacing={2}
+      sx={stackStyles}
+    >
+      <Box data-testid="DirectionalPanel-upIconBox" sx={iconBoxStyles}>
         {up ? (
           <Forward sx={upIconStyles} />
         ) : (
           <ForwardOutlined sx={upIconStyles} />
         )}
       </Box>
-      <Typography sx={textStyles}>{currentFloor}</Typography>
-      <Box sx={iconBoxStyles}>
+      <Typography data-testid="DirectionalPanel-currentFloor" sx={textStyles}>
+        {currentFloor}
+      </Typography>
+      <Box data-testid="DirectionalPanel-downIconBox" sx={iconBoxStyles}>
         {down ? (
           <Forward sx={downIconStyles} />
         ) : (
