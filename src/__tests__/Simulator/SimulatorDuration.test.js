61878358f946077f9ba0d918d9e659e27d205120
James F. Thomas <jthomas@rsimail.com>
2024-09-28 18:23:02 -0500

    SimulatorDuration all tests passing

--- src/__tests__/Simulator/SimulatorDuration.test.js
+++ src/__tests__/Simulator/SimulatorDuration.test.js
@@ -1,12 +1,50 @@
-import { renderWithProviders } from "../../utils/TestingWrapper";
-import SimulatorDuration from "../../components/simulator/SimulatorDuration";
-import { screen } from "@testing-library/react";
+import { renderWithProviders } from '../../utils/TestingWrapper';
+import { screen } from '@testing-library/react';
+import SimulatorDuration from '../../components/simulator/SimulatorDuration';
 
-//TODO add tests for START && STOP buttons
+const mockRunningState = {
+  simulator: {
+    duration: 88,
+    isRunning: true,
+    showSummary: false,
+  },
+};
 
-describe("SimulatorDuration testing suite", () => {
-  it("Component successfully renders - search by data-testid", () => {
-    renderWithProviders(<SimulatorDuration />);
-    expect(screen.getByTestId(/durationDisplayContainer/i)).toBeInTheDocument();
+const mockIdleState = {
+  simulator: {
+    duration: 129,
+    isRunning: false,
+    showSummary: false,
+  },
+};
+
+describe('SimulatorDuration testing suite', () => {
+  it('Component successfully renders', () => {
+    renderWithProviders(<SimulatorDuration />, {
+      preloadedState: mockRunningState,
+    });
+    const simulator = screen.getByTestId('durationDisplayContainer');
+    expect(simulator).toBeInTheDocument();
+  });
+  it('Component renders start button when the simulator is NOT running', () => {
+    renderWithProviders(<SimulatorDuration />, {
+      preloadedState: mockIdleState,
+    });
+    const startButton = screen.getByText('Start');
+    expect(startButton).toBeInTheDocument();
+  });
+  it('Component renders stop button when the simulator IS running', () => {
+    renderWithProviders(<SimulatorDuration />, {
+      preloadedState: mockRunningState,
+    });
+    const stopButton = screen.getByText('Stop');
+    expect(stopButton).toBeInTheDocument();
+  });
+  it('Component renders correct duration value when passed', () => {
+    renderWithProviders(<SimulatorDuration />, {
+      preloadedState: mockRunningState,
+    });
+    const durationText = screen.getByText('88');
+    expect(durationText).toBeInTheDocument();
   });
 });
