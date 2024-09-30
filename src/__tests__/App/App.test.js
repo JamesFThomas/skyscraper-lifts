4bc2721b6422e3b8a3ada5c16aac37fab91faba3
James F. Thomas <jthomas@rsimail.com>
2024-09-29 19:58:36 -0500

    App testing suite created - basic test passing

--- src/__tests__/App/App.test.js
+++ src/__tests__/App/App.test.js
@@ -1,11 +1,13 @@
-import renderer from "react-test-renderer";
-import App from "../../components/App";
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { renderWithProviders } from '../../utils/TestingWrapper';
+import App from '../../components/App';
 
 //TODO add tests for Navbar and routes
 
-describe("App testing suite", () => {
-  it("Application container successfully renders", () => {
-    const component = renderer.create(<App />);
-    expect(component).toBeTruthy();
+describe('App testing suite', () => {
+  it('Application container successfully renders', () => {
+    renderWithProviders(<App />);
+    const app = screen.getByTestId('applicationContainer');
+    expect(app).toBeInTheDocument();
   });
 });
