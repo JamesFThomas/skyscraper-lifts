9642007dda62f626be7fbb967a391eb14fc1bbe1
James F. Thomas <jthomas@rsimail.com>
2024-10-01 10:03:50 -0500

    moved router up a level to allow test wrapper to have own browserRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,36 @@
 // import dependencies
-import { Container } from "@mui/material";
+import { Container } from '@mui/material';
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 // import components
-import NavBar from "./layout/Navbar.js";
-import Home from "./pages/Home.js";
-import Auto from "./pages/Auto.js";
-import Single from "./pages/Single.js";
-import About from "./pages/About.js";
+import NavBar from './layout/Navbar.js';
+import Home from './pages/Home.js';
+import Auto from './pages/Auto.js';
+import Single from './pages/Single.js';
+import About from './pages/About.js';
 
-import { aboutData } from "../data/aboutData.js";
+import { aboutData } from '../data/aboutData.js';
 
 const App = () => {
   return (
-    <Router>
-      <Container
-        data-testid="applicationContainer"
-        maxWidth={false}
-        disableGutters
-        sx={{
-          height: "100vh",
-          backgroundColor: "lightblue",
-        }}
-      >
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auto" element={<Auto />} />
-          <Route path="/single" element={<Single />} />
-          <Route path="/about" element={<About aboutData={aboutData} />} />
-        </Routes>
-      </Container>
-    </Router>
+    <Container
+      data-testid="applicationContainer"
+      maxWidth={false}
+      disableGutters
+      sx={{
+        height: '100vh',
+        backgroundColor: 'lightblue',
+      }}
+    >
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auto" element={<Auto />} />
+        <Route path="/single" element={<Single />} />
+        <Route path="/about" element={<About aboutData={aboutData} />} />
+      </Routes>
+    </Container>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/App.js";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import store from "./state/store.js";
+import store from './state/store.js';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>,
 );
